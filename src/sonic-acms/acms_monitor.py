837078a86bc0598d4d3166e68345c96d2f763723
ganglyu <ganglv@microsoft.com>
2024-09-11 07:12:48 +0000

    Address review comment

--- src/sonic-acms/acms_monitor.py
+++ src/sonic-acms/acms_monitor.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-import os, subprocess, time, shutil, sys, re
+import datetime
+import re
+import subprocess
+import time
 from sonic_py_common import logger
 
 sonic_logger = logger.Logger()
 
 MONITOR_INTERVAL = 3600 * 24
+DSMS_CONFIG = "/var/opt/msft/client/dsms.conf"
 
 def exec_cmd(cmd, timeout=None):
     try:
@@ -22,8 +26,28 @@ def main():
         # Restart the ACMS process daily
         rc, stdoutdata, stderrdata = exec_cmd("supervisorctl status acms")
         if rc == 0 and "RUNNING" in stdoutdata:
-            sonic_logger.log_info("acms_monitor: restart acms process")
-            exec_cmd("supervisorctl restart acms")
+            poll_result = []
+            date_result = []
+            # Read next poll time from dsms.conf
+            rc, stdoutdata, stderrdata = exec_cmd("cat /var/opt/msft/client/dsms.conf")
+            if rc == 0 and "NextPollUtc" in stdoutdata:
+                pattern = r"NextPollUtc=(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2})"
+                pattern = re.compile(pattern)
+                poll_result = pattern.findall(stdoutdata)
+            # Read current date
+            rc, stdoutdata, stderrdata = exec_cmd('date "+%Y-%m-%d %H:%M:%S"')
+            if rc == 0:
+                pattern = r"(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2})"
+                pattern = re.compile(pattern)
+                date_result = pattern.findall(stdoutdata)
+            # If the next poll time exceeds one day, restart the ACMS process
+            if poll_result and date_result:
+                poll_time = datetime.datetime.strptime(poll_result[0], "%Y-%m-%d %H:%M:%S")
+                date_time = datetime.datetime.strptime(date_result[0], "%Y-%m-%d %H:%M:%S")
+                delta = date_time - poll_time
+                if delta.total_seconds() >= MONITOR_INTERVAL:
+                    sonic_logger.log_info("acms_monitor: restart acms process")
+                    exec_cmd("supervisorctl restart acms")
         time.sleep(MONITOR_INTERVAL)
 
 
--- src/sonic-acms/tests/test_acms_monitor.py
+++ src/sonic-acms/tests/test_acms_monitor.py
@@ -7,12 +7,16 @@ class TestACMSMonitor(TestCase):
     @mock.patch("time.sleep")
     def test_main_01(self, mock_sleep, mock_cmd):
         '''
-        Restart acms process if acms is running
+        Restart acms process if acms is running and next poll time expired
         Verify exec_cmd input
         '''
         expect_err = Exception("unknown error")
+        dsms_conf = "LastPollUtc=2024-01-01 04:47:40\nNextPollUtc=2024-01-01 21:26:11\nSecureBlobEtag=DEADDEAD\n"
+        current_date = "2024-01-02 22:00:00\n"
         mock_cmd.side_effect = [
             (0, "acms   RUNNING   pid 88, uptime 8 days", ""),
+            (0, dsms_conf, ""),
+            (0, current_date, ""),
             (0, "", ""),
             expect_err
         ]
@@ -22,6 +26,8 @@ class TestACMSMonitor(TestCase):
             self.assertEqual(e, expect_err)
         expected = []
         expected.append(mock.call('supervisorctl status acms'))
+        expected.append(mock.call('cat /var/opt/msft/client/dsms.conf'))
+        expected.append(mock.call('date "+%Y-%m-%d %H:%M:%S"'))
         expected.append(mock.call('supervisorctl restart acms'))
         expected.append(mock.call('supervisorctl status acms'))
         self.assertEqual(mock_cmd.call_args_list, expected)
@@ -30,6 +36,34 @@ class TestACMSMonitor(TestCase):
     @mock.patch("acms_monitor.exec_cmd")
     @mock.patch("time.sleep")
     def test_main_02(self, mock_sleep, mock_cmd):
+        '''
+        Do nothing if acms is running and next poll time not expired
+        Verify exec_cmd input
+        '''
+        expect_err = Exception("unknown error")
+        dsms_conf = "LastPollUtc=2024-01-01 04:47:40\nNextPollUtc=2024-01-01 21:26:11\nSecureBlobEtag=DEADDEAD\n"
+        current_date = "2024-01-02 21:00:00\n"
+        mock_cmd.side_effect = [
+            (0, "acms   RUNNING   pid 88, uptime 8 days", ""),
+            (0, dsms_conf, ""),
+            (0, current_date, ""),
+            expect_err
+        ]
+        try:
+            acms_monitor.main()
+        except Exception as e:
+            self.assertEqual(e, expect_err)
+        expected = []
+        expected.append(mock.call('supervisorctl status acms'))
+        expected.append(mock.call('cat /var/opt/msft/client/dsms.conf'))
+        expected.append(mock.call('date "+%Y-%m-%d %H:%M:%S"'))
+        expected.append(mock.call('supervisorctl status acms'))
+        self.assertEqual(mock_cmd.call_args_list, expected)
+
+
+    @mock.patch("acms_monitor.exec_cmd")
+    @mock.patch("time.sleep")
+    def test_main_03(self, mock_sleep, mock_cmd):
         '''
         Do nothing if acms is not running
         Verify exec_cmd input
