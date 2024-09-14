7253b8ba50f583b84c25340c94b2c1601445638f
ganglyu <ganglv@microsoft.com>
2024-09-14 02:23:57 +0000

    Address review comment

--- src/sonic-acms/acms_monitor.py
+++ src/sonic-acms/acms_monitor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 import re
 import subprocess
 import time
@@ -19,33 +20,32 @@ def exec_cmd(cmd, timeout=None):
         return -1, "", ""
     return res.returncode, res.stdout.decode(), res.stderr.decode()
 
+def get_now_time():
+    return datetime.datetime.now()
+
 def main():
     sonic_logger.set_min_log_priority_info()
 
     while True:
-        # Restart the ACMS process daily
+        # Monitor the ACMS process and restart it if it becomes stuck
         rc, stdoutdata, stderrdata = exec_cmd("supervisorctl status acms")
         if rc == 0 and "RUNNING" in stdoutdata:
             poll_result = []
-            date_result = []
             # Read next poll time from dsms.conf
-            rc, stdoutdata, stderrdata = exec_cmd("cat /var/opt/msft/client/dsms.conf")
-            if rc == 0 and "NextPollUtc" in stdoutdata:
-                pattern = r"NextPollUtc=(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2})"
-                pattern = re.compile(pattern)
-                poll_result = pattern.findall(stdoutdata)
-            # Read current date
-            rc, stdoutdata, stderrdata = exec_cmd('date +%Y-%m-%d:%H:%M:%S')
-            if rc == 0:
-                pattern = r"(\d{4}-\d{1,2}-\d{1,2}:\d{1,2}:\d{1,2}:\d{1,2})"
-                pattern = re.compile(pattern)
-                date_result = pattern.findall(stdoutdata)
+            if os.path.exists(DSMS_CONFIG):
+                with open(DSMS_CONFIG) as fp:
+                    text = fp.read()
+                    if "NextPollUtc" in text:
+                        pattern = r"NextPollUtc=(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2})"
+                        pattern = re.compile(pattern)
+                        poll_result = pattern.findall(text)
             # If the next poll time exceeds one day, restart the ACMS process
-            if poll_result and date_result:
+            if poll_result:
                 sonic_logger.log_info("acms_monitor: next poll time is " + poll_result[0])
-                sonic_logger.log_info("acms_monitor: current time is " + date_result[0])
-                poll_time = datetime.datetime.strptime(poll_result[0], "%Y-%m-%d %H:%M:%S")
-                date_time = datetime.datetime.strptime(date_result[0], "%Y-%m-%d:%H:%M:%S")
+                # Timezone is UTC
+                time_str = poll_result[0] + " UTC"
+                poll_time = datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S %Z")
+                date_time = get_now_time()
                 delta = date_time - poll_time
                 if delta.total_seconds() >= MONITOR_INTERVAL:
                     sonic_logger.log_info("acms_monitor: restart acms process")
