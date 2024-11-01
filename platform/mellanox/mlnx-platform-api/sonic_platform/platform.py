e04db68452a4a02e70dff2f72f0ed2071452eabb
gpunathilell <gpunathilell@nvidia.com>
2024-11-01 05:23:18 +0200

    Platform API Commit

--- platform/mellanox/mlnx-platform-api/sonic_platform/platform.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/platform.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019-2021 NVIDIA CORPORATION & AFFILIATES.
+# Copyright (c) 2019-2024 NVIDIA CORPORATION & AFFILIATES.
 # Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,7 +22,7 @@
 
 try:
     from sonic_platform_base.platform_base import PlatformBase
-    from .chassis import Chassis, ModularChassis
+    from .chassis import Chassis, ModularChassis, SmartSwitchChassis
     from .device_data import DeviceDataManager
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
@@ -30,7 +30,9 @@ except ImportError as e:
 class Platform(PlatformBase):
     def __init__(self):
         PlatformBase.__init__(self)
-        if DeviceDataManager.get_linecard_count() == 0:
+        if DeviceDataManager.get_dpu_count():
+            self._chassis = SmartSwitchChassis()
+        elif DeviceDataManager.get_linecard_count() == 0:
             self._chassis = Chassis()
         else:
             self._chassis = ModularChassis()
