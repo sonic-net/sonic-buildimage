a6d0fa34cae65016486b528cc929223e5bdc0188
Kavitha Ramalingam <ramalingam_k@hcl.com>
2024-11-14 16:21:30 +0530

    gNOI Warm Reboot - Added tests

--- src/sonic-framework/rebootbackend/rebootbe.cpp
+++ src/sonic-framework/rebootbackend/rebootbe.cpp
@@ -31,7 +31,6 @@ RebootBE::RebootBE(DbusInterface &dbus_interface)
 }
 
 RebootBE::RebManagerStatus RebootBE::GetCurrentStatus() {
-  const std::lock_guard<std::mutex> lock(m_StatusMutex);
   return m_CurrentStatus;
 }
 
@@ -43,6 +42,9 @@ void RebootBE::SetCurrentStatus(RebManagerStatus newStatus) {
 void RebootBE::Start() {
   SWSS_LOG_ENTER();
   SWSS_LOG_NOTICE("--- Starting rebootbackend ---");
+  swss::WarmStart::initialize("rebootbackend", "sonic-framework");
+  swss::WarmStart::checkWarmStart("rebootbackend", "sonic-framework",
+                                  /*incr_restore_cnt=*/false);
 
   swss::Select s;
   s.addSelectable(&m_NotificationConsumer);
@@ -175,9 +177,9 @@ bool RebootBE::RebootAllowed(const gnoi::system::RebootMethod rebMethod) {
     case RebManagerStatus::WARM_REBOOT_IN_PROGRESS: {
       return false;
     }
-    case RebManagerStatus::WARM_INIT_WAIT: {
+     case RebManagerStatus::WARM_INIT_WAIT: {
       return rebMethod == gnoi::system::RebootMethod::COLD;
-    }
+    } 
     case RebManagerStatus::IDLE: {
       return true;
     }
