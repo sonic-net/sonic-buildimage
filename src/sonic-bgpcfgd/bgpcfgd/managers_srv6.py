3b8b9b72217ef7697f7b20f8abe4716124d3b6d0
BYGX-wcr <wcr@live.cn>
2024-12-04 00:49:00 +0000

    complete the set_handler and del_handler in the srv6 manager

--- src/sonic-bgpcfgd/bgpcfgd/managers_srv6.py
+++ src/sonic-bgpcfgd/bgpcfgd/managers_srv6.py
@@ -4,7 +4,18 @@ from .manager import Manager
 from .template import TemplateFabric
 import socket
 from swsscommon import swsscommon
-from ipaddress import ip_network, IPv6Network
+from ipaddress import IPv6Network, IPv6Address
+
+supported_SRv6_behaviors = {
+    'end',
+    'end.dt6',
+    'end.dt46',
+    'end.x',
+    'uN',
+    'uDT6',
+    'uDT46',
+    'uA'
+}
 
 class SRv6Mgr(Manager):
     """ This class updates SRv6 configurations when SRV6_MY_SID_TABLE table is updated """
@@ -23,15 +34,41 @@ class SRv6Mgr(Manager):
         )
 
         self.sids = {} # locators -> SIDs
-        self.config_db = None
+        self.config_db = swsscommon.SonicV2Connector()
+        self.config_db.connect(self.config_db.CONFIG_DB)
 
     def set_handler(self, key, data):
         ip_addr = key
+        if 'action' not in data:
+            log_err("Found a SRv6 config entry that does not specify action: {} | {}".format(ip_addr, data))
+            return
+        
+        if data['action'] not in supported_SRv6_behaviors:
+            log_err("Found a SRv6 config entry associated with unsupported action: {} | {}".format(ip_addr, data))
+            return
+        
+        if 'vrf' in data:
+            # verify that vrf name exists in the VRF_TABLE of CONFIG_DB
+            if not self.config_db.exists(self.config_db.CONFIG_DB, "VRF_TABLE|{}".format(data['vrf'])):
+                log_err("Found a SRv6 config entry that maps to an undefined VRF: {} | {}".format(ip_addr, data))
+                return
+        
         sid = SID(ip_addr, data)
         locator = sid.get_locator()
         opcode = sid.get_opcode()
 
-        # TODO: generate FRR commands and push to Config Manager
+        cmd_list = ['segment-routing', 'srv6']
+        cmd_list += ['locators', 'locator {}'.format(locator)]
+        if locator not in self.sids:
+            cmd_list += ['prefix {}/{} block-len {} node-len {}'.format(locator, sid.block_len + sid.node_len, sid.block_len, sid.node_len)]
+
+        opcode_cmd = 'opcode ::{} {}'.format(opcode, sid.action)
+        if sid.vrf != 'default':
+            opcode_cmd += "vrf " + sid.vrf
+        cmd_list.append(opcode_cmd)
+
+        self.cfg_mgr.push_list(cmd_list)
+        log_debug("{} SRv6 static configuration {} is scheduled for updates. {}".format(self.db_name, key, str(cmd_list)))
 
         self.sids.setdefault(locator, {})[opcode] = sid
 
@@ -42,10 +79,30 @@ class SRv6Mgr(Manager):
         opcode = sid.get_opcode()
 
         if locator in self.sids:
-            #TODO: delete FRR opcode config
-            pass
+            if opcode not in self.sids[locator]:
+                log_warn("Encountered a config deletion with an unexpected SRv6 opcode: {} | {}".format(ip_addr, data))
+                return
+            
+            cmd_list = ['segment-routing', 'srv6']
+            cmd_list.append('locators')
+            if len(self.sids[locator] == 1):
+                # this is the last opcode of the locator, so we should delete the whole locator
+                cmd_list.append('no locator {}'.format(locator))
+
+                self.sids.pop(locator)
+            else:
+                # delete this opcode only
+                opcode_cmd = 'no opcode ::{} {}'.format(opcode, sid.action)
+                if sid.vrf != 'default':
+                    opcode_cmd += "vrf " + sid.vrf
+                cmd_list.append(opcode_cmd)
+
+                self.sids[locator].pop(opcode)
+
+            self.cfg_mgr.push_list(cmd_list)
+            log_debug("{} SRv6 static configuration {} is scheduled for updates. {}".format(self.db_name, key, str(cmd_list)))
         else:
-            log_warn("Encountered an unexpected config change")
+            log_warn("Encountered a config deletion with an unexpected SRv6 locator: {} | {}".format(ip_addr, data))
             return
 
 class SID:
@@ -62,7 +119,7 @@ class SID:
             locator_mask <<= 1
             locator_mask |= 0x01
         locator_mask <<= 128 - (self.block_len + self.node_len)
-        self.locator = self.bits & locator_mask
+        self.locator = IPv6Address(self.bits & locator_mask)
 
         # extract the opcode(function id)
         func_mask = 0
@@ -70,11 +127,7 @@ class SID:
             func_mask <<= 1
             func_mask != 0x01
         func_mask <<= 128 - (self.block_len + self.node_len + self.func_len)
-        self.opcode = self.bits & func_mask
-
-        if 'action' not in data:
-            log_err("Found a SRv6 config entry that does not specify action: {}|{}".format(ip_addr, data))
-            raise RuntimeError("SID creation encountered error!")
+        self.opcode = IPv6Address(self.bits & func_mask)
         
         self.action = data['action']
         self.vrf = data['vrf'] if 'vrf' in data else "default"
