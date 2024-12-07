72e4b1c22f440ddd31d3731881ce727101e3d8cf
Prabhat Aravind <paravind@microsoft.com>
2024-12-07 06:05:36 +0000

    Enable ZMQ between orchagent and syncd for smartswitch DPUs
     * Add device type to swss_vars template
     * Set redis communication mode to zmq_sync for SmartSwitchDPU device type
     * Add a context configuration to enable zmq mode for Smartswitch DPU as per
       https://github.com/sonic-net/sonic-sairedis/wiki/Context-configuration
    
    Signed-off-by: Prabhat Aravind <paravind@microsoft.com>

--- dockers/docker-orchagent/orchagent.sh
+++ dockers/docker-orchagent/orchagent.sh
@@ -28,9 +28,13 @@ else
     ORCHAGENT_ARGS+="-b 1024 "
 fi
 
-# Set synchronous mode if it is enabled in CONFIG_DB
+# Set zmq mode by default for smartswitch DPU
+# Otherwise, set synchronous mode if it is enabled in CONFIG_DB
 SYNC_MODE=$(echo $SWSS_VARS | jq -r '.synchronous_mode')
-if [ "$SYNC_MODE" == "enable" ]; then
+DEVICE_TYPE=$(echo $SWSS_VARS | jq -r '.type')
+if [ "$DEVICE_TYPE" == "SmartSwitchDPU" ]; then
+    ORCHAGENT_ARGS+="-z zmq_sync "
+elif [ "$SYNC_MODE" == "enable" ]; then
     ORCHAGENT_ARGS+="-s "
 fi
 
