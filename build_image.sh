e12d2017aed5d49fa279f11e6a7e0e5d8b8951d5
Nazarii Hnydyn <nazariig@nvidia.com>
2024-12-12 16:13:21 +0200

    [mellanox]: Add KVM image target
    Signed-off-by: Nazarii Hnydyn <nazariig@nvidia.com>

--- build_image.sh
+++ build_image.sh
@@ -113,9 +113,13 @@ generate_device_list()
         fi;
     done
 
-    # Add kvm to the list
-    if [ "$TARGET_MACHINE" = "alpinevs" ] ; then
-      echo "x86_64-kvm_x86_64-r0" >> "$platforms_asic";
+    # Inject KVM platform info for Mellanox build
+    if [ "$MLNX_KVM_IMAGE" = "yes" ]; then
+        for d in `find -L ./device/virtual -maxdepth 1 -mindepth 1 -type d`; do
+            if [ -f "$d/platform_asic" ]; then
+                echo "${d##*/}" >> "$platforms_asic";
+            fi
+        done
     fi
 }
 
@@ -167,6 +171,9 @@ elif [ "$IMAGE_TYPE" = "raw" ]; then
     echo "The raw image is in $OUTPUT_RAW_IMAGE"
 
 elif [ "$IMAGE_TYPE" = "kvm" ]; then
+    if [ "$TARGET_MACHINE" = "mellanox" ]; then
+        export MLNX_KVM_IMAGE="yes"
+    fi
 
     generate_device_list "./installer/platforms_asic"
 
