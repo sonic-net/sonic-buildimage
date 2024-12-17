cb538b53919c5a00653f1854b13f8088a1edcf54
Wataru Ishida <wataru.ishid@gmail.com>
2024-12-17 07:09:43 +0000

    vs: extend platform_module to emulate xcvrs
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- platform/vs/sonic-platform-modules-vs/sonic_platform/chassis.py
+++ platform/vs/sonic-platform-modules-vs/sonic_platform/chassis.py
@@ -6,11 +6,22 @@
 #
 
 try:
-    from sonic_platform_base.chassis_base import ChassisBase
-    from sonic_platform_base.module_base import ModuleBase
     import os
+    import time
     import json
-    import threading
+    import sys
+
+    import grpc
+
+    from xcvr_emu.proto import emulator_pb2 as pb2
+    # see https://github.com/grpc/grpc/issues/29459#issuecomment-1641587881
+    proto_dir = os.path.dirname(pb2.__file__)
+    sys.path.append(proto_dir)
+
+    from xcvr_emu.proto import emulator_pb2_grpc
+
+    from sonic_platform_base.chassis_base import ChassisBase
+    from .sfp import Sfp
 
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
@@ -24,6 +35,9 @@ class Chassis(ChassisBase):
         ChassisBase.__init__(self)
         self.metadata_file = '/etc/sonic/vs_chassis_metadata.json'
         self.metadata = self._read_metadata()
+        channel = grpc.insecure_channel("localhost:50051")
+        self.xcvr_emu = emulator_pb2_grpc.SfpEmulatorServiceStub(channel)
+        self.sfps = {}
 
     def _read_metadata(self):
         metadata = {}
@@ -53,3 +67,34 @@ class Chassis(ChassisBase):
         else:
             raise ValueError("Invalid configuration: Neither supervisor nor line card")
 
+    def get_sfp(self, index):
+        if index not in self.sfps:
+            sfp = Sfp(index, self.xcvr_emu)
+            self.sfps[index] = (sfp, sfp.get_presence())
+
+        return self.sfps[index][0]
+
+    def get_change_event(self, timeout=0):
+
+        port_dict = {}
+        change_dict = {"sfp": port_dict}
+
+        start = time.time()
+
+        while True:
+            for (sfp, present) in self.sfps.values():
+                current = sfp.get_presence()
+                if current != present:
+                    port_dict[sfp.index] = '1' if current else '0'
+                    self.sfps[sfp.index] = (sfp, current)
+
+            if len(port_dict):
+                return True, change_dict
+
+            if timeout > 0 and (time.time() - start > (timeout / 1000)):
+                return True, change_dict
+
+            time.sleep(0.5)
+
+    def get_reboot_cause(self):
+        return ("REBOOT_CAUSE_NON_HARDWARE", "Unknown")
