a33506761cf78008b6b4811454eeda46a8aed8c1
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-01-07 14:28:48 +0800

    Update sfputil.py

--- device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfputil.py
+++ device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfputil.py
@@ -212,7 +212,7 @@ class SfpUtil(SfpUtilBase):
             from swsscommon import swsscommon
             self.state_db = swsscommon.DBConnector("STATE_DB",
                                                    REDIS_TIMEOUT_USECS,
-                                                   True)
+                                                   False)
 
             # Subscribe to state table for SFP change notifications
             self.db_sel = swsscommon.Select()
