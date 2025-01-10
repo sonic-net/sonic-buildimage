edb64f7c07756e8f92886986469a8705060c9c54
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-01-10 15:06:01 +0800

    Change DBConnector parameter to True

--- device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfputil.py
+++ device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfputil.py
@@ -212,7 +212,7 @@ class SfpUtil(SfpUtilBase):
             from swsscommon import swsscommon
             self.state_db = swsscommon.DBConnector("STATE_DB",
                                                    REDIS_TIMEOUT_USECS,
-                                                   False)
+                                                   True)
 
             # Subscribe to state table for SFP change notifications
             self.db_sel = swsscommon.Select()
