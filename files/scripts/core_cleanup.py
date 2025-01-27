665605ccda2e95e86c6c6bdabc1bc4275b37d224
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-01-27 13:32:22 +0800

    Refactor core and kernel dump cleanup

--- files/scripts/core_cleanup.py
+++ files/scripts/core_cleanup.py
@@ -3,7 +3,7 @@
 import os
 import re
 from collections import defaultdict
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 from sonic_py_common.logger import Logger
 
@@ -23,7 +23,7 @@ def get_dump_timestamp(file_name):
     match = re.search(r'sonic_dump_.*_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d).tar.gz', file_name)
     if match:
         groups = match.groups()
-        dump_date = datetime(int(groups[0]), int(groups[1]), int(groups[2]), int(groups[3]), int(groups[4]), int(groups[5]))
+        return datetime(int(groups[0]), int(groups[1]), int(groups[2]), int(groups[3]), int(groups[4]), int(groups[5]))
     
     return None
 
@@ -34,8 +34,8 @@ def main():
     if os.getuid() != 0:
         logger.log_error('Root required to clean up core files')
         return
-        
-    expire_date = date.today() + timedelta(days=EXPIRE_DAYS)
+
+    expire_date = datetime.now() - timedelta(days=EXPIRE_DAYS)
 
     logger.log_info('Cleaning up core files')
     core_files = [f for f in os.listdir(CORE_FILE_DIR) if os.path.isfile(os.path.join(CORE_FILE_DIR, f))]
@@ -61,11 +61,12 @@ def main():
             core_files_by_process[process] = curr_files[0:MAX_CORE_FILES]
 
     logger.log_info('Finished cleaning up core files')
-    
+
     # cleanup kernel dumps
     logger.log_info('Cleaning up kernel dump files')
-
     kernel_dumps = [f for f in os.listdir(KERNEL_DUMP_DIR) if os.path.isfile(os.path.join(KERNEL_DUMP_DIR, f))]
+    # Sort kernel dumps from new to old
+    kernel_dumps.sort(reverse = True, key = lambda x: get_dump_timestamp(x))
     not_expired_dumps = []
     for kernel_dump in kernel_dumps:
         # delete expired kernel dump
@@ -79,13 +80,12 @@ def main():
             delete_dump(os.path.join(KERNEL_DUMP_DIR, kernel_dump))
             continue
 
-        not_expired_dumps.append(f)
-        if len(not_expired_dumps) > MAX_CORE_FILES:
-            not_expired_dumps.sort(reverse = True, key = lambda x: get_dump_timestamp(kernel_dump))
-            oldest_dump = not_expired_dumps[MAX_CORE_FILES]
-            logger.log_info('Deleting {}'.format(oldest_dump))
-            delete_dump(os.path.join(KERNEL_DUMP_DIR, oldest_dump))
-            not_expired_dumps = not_expired_dumps[0:MAX_CORE_FILES]
+        # Only keep recent MAX_CORE_FILES kernel dumps
+        if len(not_expired_dumps) < MAX_CORE_FILES:
+            not_expired_dumps.append(kernel_dump)
+        else:
+            logger.log_info('Deleting {}'.format(kernel_dump))
+            delete_dump(os.path.join(KERNEL_DUMP_DIR, kernel_dump))
 
     logger.log_info('Finished cleaning up kernel dump files')
 
