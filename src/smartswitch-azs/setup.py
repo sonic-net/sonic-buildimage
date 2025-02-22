f9cbe5f42b1a5a07bd1a86edbc4ecab7a1ef125f
Mia Tao <miatao@microsoft.com>
2025-02-21 18:04:37 -0800

    set up python sctipts

---
+++ src/smartswitch-azs/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+
+# read me
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Anf Rotterdam",
+    author_email='anishsdirects@microsoft.com',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Package contains Python scripts for smartswitch-azs for SONiC.",
+    license="GNU General Public License v3",
+        long_description=readme + '\n\n',
+    scripts=[
+        'bgp_session_tracker.py',
+        'link_state_tracker.py'
+    ],
+    install_requires = [
+        'pyroute2==0.7.2',
+        'netifaces==0.11.0',
+        'sonic-py-common'
+    ],
+    tests_require = [
+        'pytest>3',
+        'xmltodict==0.12.0',
+        'ijson==2.6.1'
+    ],
+    extras_require = {
+        'testing': [
+            'pytest>3',
+            'xmltodict==0.12.0',
+            'ijson==2.6.1'
+        ],
+    },
+    setup_requires = [
+        'pytest-runner',
+        'wheel'
+    ],
+    include_package_data=True,
+    keywords='sonic-smartswitch-azs',
+    name='sonic-smartswitch-azs',
+    # py_modules=['configutil'],
+    # packages=find_packages(),
+    version='1.0',
+    zip_safe=False,
+    entry_point = {
+        'console_scripts' : [
+            'bgp-session-tracker=smartswitch_azs.bgp_session_tracker:main',
+            'link-state-tracker=smartswitch_azs.link_state_tracker:main'
+        ]
+    }
+)
