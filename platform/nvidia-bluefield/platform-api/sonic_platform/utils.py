5b79f48720b17a10ceb4689a56984707adc3a2d6
gpunathilell <gpunathilell@nvidia.com>
2025-02-24 19:28:17 +0200

    Added watchdog support for bluefield

--- platform/nvidia-bluefield/platform-api/sonic_platform/utils.py
+++ platform/nvidia-bluefield/platform-api/sonic_platform/utils.py
@@ -71,3 +71,64 @@ def default_return(return_value, log_func=logger.log_debug):
                 return return_value
         return _impl
     return wrapper
+
+def read_from_file(file_path, target_type, default='', raise_exception=False, log_func=logger.log_error):
+    """
+    Read content from file and convert to target type
+    :param file_path: File path
+    :param target_type: target type
+    :param default: Default return value if any exception occur
+    :param raise_exception: Raise exception to caller if True else just return default value
+    :param log_func: function to log the error
+    :return: String content of the file
+    """
+    try:
+        with open(file_path, 'r') as f:
+            value = f.read()
+            if value is None:
+                # None return value is not allowed in any case, so we log error here for further debug.
+                logger.log_error('Failed to read from {}, value is None, errno is {}'.format(file_path, ctypes.get_errno()))
+                # Raise ValueError for the except statement to handle this as a normal exception
+                raise ValueError('File content of {} is None'.format(file_path))
+            else:
+                value = target_type(value.strip())
+    except (ValueError, IOError) as e:
+        if log_func:
+            log_func('Failed to read from file {} - {}'.format(file_path, repr(e)))
+        if not raise_exception:
+            value = default
+        else:
+            raise e
+
+    return value
+
+def read_float_from_file(file_path, default=0.0, raise_exception=False, log_func=logger.log_error):
+    """
+    Read content from file and cast it to integer
+    :param file_path: File path
+    :param default: Default return value if any exception occur
+    :param raise_exception: Raise exception to caller if True else just return default value
+    :param log_func: function to log the error
+    :return: Integer value of the file content
+    """
+    return read_from_file(file_path=file_path, target_type=float, default=default, raise_exception=raise_exception, log_func=log_func)
+
+def write_file(file_path, content, raise_exception=False, log_func=logger.log_error):
+    """
+    Write the given value to a file
+    :param file_path: File path
+    :param content: Value to write to the file
+    :param raise_exception: Raise exception to caller if True
+    :return: True if write success else False
+    """
+    try:
+        with open(file_path, 'w') as f:
+            f.write(str(content))
+    except (ValueError, IOError) as e:
+        if log_func:
+            log_func('Failed to write {} to file {} - {}'.format(content, file_path, repr(e)))
+        if not raise_exception:
+            return False
+        else:
+            raise e
+    return True
