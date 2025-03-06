5f5a5c174f0e0f0f260eca9d1c4e3252061d196e
Nina Wang <wangnina@microsoft.com>
2025-03-05 17:47:23 -0800

    Fix dummy test

--- src/smartswitch-azs/tests/test_basic.py
+++ src/smartswitch-azs/tests/test_basic.py
@@ -5,22 +5,8 @@ import unittest
 import sys
 import os
 
-# Add the parent directory to import modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-# Import the modules.
-try:
-    import smartswitch-azs
-    SMARTSWITCH_IMPORTED = True
-except ImportError:
-    SMARTSWITCH_IMPORTED = False
-
 class TestSmartSwitchAZS(unittest.TestCase):
     """Basic test class for smartswitch-azs"""
-
-    def test_imports(self):
-        """Test modules can be imported"""
-        self.assertTrue(SMARTSWITCH_IMPORTED, "smartswitch-azs module imported successsfully.")
     
     def test_dummy(self):
         self.assertTrue(True, "Dummy test passed.")
