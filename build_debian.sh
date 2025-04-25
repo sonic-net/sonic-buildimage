28790851afaf8f284482dea60cf17166d04c5883
Yutong Zhang <yutongzhang@microsoft.com>
2025-04-25 09:51:57 +0800

    Remove install PyYAML

--- build_debian.sh
+++ build_debian.sh
@@ -528,8 +528,6 @@ sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'docke
 # Install scapy
 sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'scapy==2.4.4'
 
-sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'PyYAML==6.0.1'
-
 ## Note: keep pip installed for maintainance purpose
 
 # Install GCC, needed for building/installing some Python packages
