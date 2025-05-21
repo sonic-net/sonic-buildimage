76ecf29c62a37b842a5bf71b17e30dd9cd4d13ed
ganglyu <ganglv@microsoft.com>
2025-05-21 12:44:44 +0800

    Update file name

--- rules/docker-gnmi.mk
+++ rules/docker-gnmi.mk
@@ -37,7 +37,7 @@ $(DOCKER_GNMI)_RUN_OPT += -v /var/run/dbus:/var/run/dbus:rw
 
 $(DOCKER_GNMI)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
 ifeq ($(SMARTSWITCH), 1)
-$(DOCKER_GNMI)_BASE_IMAGE_FILES += monit_gnmi_smartswitch:/etc/monit/conf.d
+$(DOCKER_GNMI)_BASE_IMAGE_FILES += monit_gnmi_smartswitch:/etc/monit/conf.d/monit_gnmi
 else
-$(DOCKER_GNMI)_BASE_IMAGE_FILES += monit_gnmi:/etc/monit/conf.d
+$(DOCKER_GNMI)_BASE_IMAGE_FILES += monit_gnmi:/etc/monit/conf.d/monit_gnmi
 endif
