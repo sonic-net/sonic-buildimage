51bb89d66f544d7cd776af271d9e8ad5e6179081
ganglyu <ganglv@microsoft.com>
2025-06-11 11:08:32 +0800

    Enable user_auth by default

--- dockers/docker-sonic-gnmi/gnmi-native.sh
+++ dockers/docker-sonic-gnmi/gnmi-native.sh
@@ -124,6 +124,10 @@ else
 fi
 
 USER_AUTH=$(extract_field "$GNMI" '.user_auth')
+# If user_auth is not set, default to certs
+if [ $USER_AUTH == "null" ]; then
+    USER_AUTH="certs"
+fi
 if [ ! -z "$USER_AUTH" ] && [  $USER_AUTH != "null" ]; then
     TELEMETRY_ARGS+=" --client_auth $USER_AUTH"
 
