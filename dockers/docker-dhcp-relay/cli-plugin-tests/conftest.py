7777780c19ee06061d410b6fa9e85a7915cda5dc
Shivashankar CR <shivashankar.c.r@gmail.com>
2025-07-06 23:46:28 -0700

    Revert CLI changes, Moved to new PR

--- dockers/docker-dhcp-relay/cli-plugin-tests/conftest.py
+++ dockers/docker-dhcp-relay/cli-plugin-tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import mock_tables # lgtm [py/unused-import]
 from unittest import mock
+
 @pytest.fixture()
 def mock_cfgdb():
     cfgdb = mock.Mock()
@@ -8,57 +9,12 @@ def mock_cfgdb():
         'VLAN': {
             'Vlan1000': {
                 'dhcp_servers': ['192.0.0.1']
-            },
-            'Vlan200': {},
-        },
-        'DHCPV4_RELAY': {
-            'Vlan1000': {
-                'dhcpv4_servers': ['192.0.0.1']
-            },
-            'Vlan200': {},
+            }
         },
         'DHCP_RELAY': {
             'Vlan1000': {
                 'dhcpv6_servers': ['fc02:2000::1']
             }
-        },
-        'FEATURE': {
-            'dhcp_relay': {
-                'has_sonic_dhcpv4_relay' : "False"
-            }
-        },
-        'VRF': {
-            "default": {
-                'VRF': 'default'
-            },
-            "VrfRED": {
-                'VRF': 'VrfRED'
-            },
-            "VrfBLUE": {
-                'VRF': 'VrfBLUE'
-            }
-        },
-        'PORTCHANNEL_INTERFACE': {
-            "PortChannel5": {},
-            "PortChannel6": {},
-            "PortChannel5|192.168.0.1/31": {},
-            "PortChannel6|192.168.0.3/31": {}
-        },
-        'LOOPBACK_INTERFACE': {
-            "Loopback0": {},
-            "Loopback2": {},
-            "Loopback3": {},
-            "Loopback0|10.1.0.1/32": {},
-            "Loopback2|10.1.0.1/32": {},
-            "Loopback3|10.1.0.2/32": {}
-        },
-        'INTERFACE': {
-            "Ethernet0": {},
-            "Ethernet0|10.0.0.0/31": {},
-            "Ethernet8": {},
-            "Ethernet8|10.0.0.4/31": {},
-            "Ethernet12": {},
-            "Ethernet12|10.0.0.6/31": {},
         }
     }
 
@@ -78,10 +34,6 @@ def mock_cfgdb():
     def get_keys(table):
         return CONFIG[table].keys()
 
-    def get_table(table):
-        return CONFIG.get(table, {})
-
-    cfgdb.get_table = mock.Mock(side_effect=get_table)
     cfgdb.get_entry = mock.Mock(side_effect=get_entry)
     cfgdb.set_entry = mock.Mock(side_effect=set_entry)
     cfgdb.get_keys = mock.Mock(side_effect=get_keys)
