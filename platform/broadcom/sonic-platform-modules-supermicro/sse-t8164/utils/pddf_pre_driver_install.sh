3fe393aa85c22f374cc59bd199c7c83d3ef0e86d
Julian Chang - TW <julianc@supermicro.com.tw>
2025-07-10 08:22:32 +0000

    Revert "[SuperMicro] Adding new platform SSE-T8164 and SSE-T8196."
    This reverts commit 08e90911a27f9da7e7453ebed11829676b0b5a77.

--- platform/broadcom/sonic-platform-modules-supermicro/sse-t8164/utils/pddf_pre_driver_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-modprobe i2c-i801_t8164
-modprobe i2c-ismt bus_speed=400
-modprobe ast
--- platform/broadcom/sonic-platform-modules-supermicro/sse-t8196/utils/pddf_pre_driver_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-modprobe i2c-i801
-modprobe ast
-modprobe i2c-ismt delay=100
-modprobe i2c-dev
