8d4c248cd5b2f692751cad676d9fba7d39a9b84a
david.zagury <davidza@nvidia.com>
2025-07-13 17:46:38 +0300

    Move mlx paths to only be on mlx

--- rules/docker-platform-monitor.mk
+++ rules/docker-platform-monitor.mk
@@ -53,7 +53,6 @@ $(DOCKER_PLATFORM_MONITOR)_CONTAINER_NAME = pmon
 # $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += --privileged -t
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += --cap-add=SYS_RAWIO --cap-add=SYS_ADMIN -t --security-opt apparmor=unconfined --security-opt="systempaths=unconfined"
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += --device=/dev/sda:/dev/sda
-$(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /sys/module/sx_core:/sys/module/sx_core:rw
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /etc/localtime:/etc/localtime:ro 
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /host/reboot-cause:/host/reboot-cause:rw
@@ -61,7 +60,6 @@ $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /host/pmon/stormond:/usr/share/stormond
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /var/run/platform_cache:/var/run/platform_cache:ro
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/share/sonic/device/pddf:/usr/share/sonic/device/pddf:ro
 # Add LED device mounts for hardware access
-$(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /sys/devices/platform/mlxplat:/sys/devices/platform/mlxplat:rw
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /sys/class/leds:/sys/class/leds:rw
 
 # Mount Arista python library on Aboot images to be used by plugins
