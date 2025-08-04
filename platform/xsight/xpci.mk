d08ba056969fbb81d5d2b3177554b241cab89df1
Anton Parkhomenko <aparkhomenko@larch-networks.com>
2025-08-04 10:27:53 +0000

    [Xsight] Update XPCI driver to use DKMS infrastructure
    - Why I did it
    To always build drivers from sources and don't use pre-built drivers
    
    - How I did it
    DKMS modules xpci-dkms_0.7.0_amd64.deb is saved and then used to build
    the binary module
    
    - How to verify it
    Make sure compilation and basic tests are successful
    
    BLDENV=bookworm make -f Makefile.work \
    	target/debs/bookworm/xpci-modules-6.1.0-29-2-amd64_0.7.0_amd64.deb
    
    Signed-off-by: Anton Parkhomenko <aparkhomenko@larch-networks.com>

--- platform/xsight/xpci.mk
+++ platform/xsight/xpci.mk
@@ -4,8 +4,13 @@ XPCI_VERSION = 1.0
 
 export XPCI_VERSION
 
-XPCI = xpci-modules-$(KVERSION)_$(XPCI_VERSION)_amd64.deb
+XSIGHT_XPCI_URL_PREFIX = "https://raw.githubusercontent.com/xsightlabs/SONiC/main/aparkhomenko-dev/amd64/kernel/"
+XPCI = xpci-dkms_$(XPCI_VERSION)_amd64.deb
+$(XPCI)_URL = "$(XSIGHT_XPCI_URL_PREFIX)/$(XPCI)"
+SONIC_ONLINE_DEBS += $(XPCI)
 
-$(XPCI)_SRC_PATH = $(PLATFORM_PATH)/xpci
-SONIC_MAKE_DEBS += $(XPCI)
+KERNEL_XPCI = xpci-modules-$(KVERSION)_$(XPCI_VERSION)_amd64.deb
+$(KERNEL_XPCI)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON) $(XPCI)
 
+$(KERNEL_XPCI)_SRC_PATH = $(PLATFORM_PATH)/xpci
+SONIC_MAKE_DEBS += $(KERNEL_XPCI)
