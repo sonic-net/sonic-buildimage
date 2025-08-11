6a3c0d0bdaf261bcce7138daf78846164d245bbb
Janetxxx <janet970527@gmail.com>
2025-08-11 22:35:24 +1000

    update otel vars
    Signed-off-by: Janetxxx <janet970527@gmail.com>

--- dockers/docker-sonic-otel/otel.sh
+++ dockers/docker-sonic-otel/otel.sh
@@ -1,90 +1,24 @@
 #!/usr/bin/env bash
 
 EXIT_OTEL_VARS_FILE_NOT_FOUND=1
-INCORRECT_OTEL_VALUE=2
 OTEL_VARS_FILE=/usr/share/sonic/templates/otel_vars.j2
 CONFIG_FILE=/etc/otel-collector/config.yaml
 
-extract_field() {
-    echo $(echo $1 | jq -r $2)
-}
-
 if [ ! -f "$OTEL_VARS_FILE" ]; then
-    echo "OTEL vars template file not found, using default config"
-    # Use your exact local configuration as default
-    cat > $CONFIG_FILE << 'EOF'
-receivers:
-  otlp:
-    protocols:
-      grpc:
-        endpoint: "0.0.0.0:4317"
-      http:
-        endpoint: "0.0.0.0:4318"
-
-processors:
-  batch:
-    timeout: 1s
-    send_batch_size: 10
-
-  filter/ports:
-    metrics:
-      include:
-        match_type: regexp
-        metric_names:
-          - port\..*
-
-  filter/buffers:
-    metrics:
-      include:
-        match_type: regexp
-        metric_names:
-          - buffer_pool\..*
-
-exporters:
-  influxdb/ports:
-    endpoint: http://influxdb-a:8086
-    org: docs
-    bucket: ports
-    token: mytoken123456789
-    timeout: 10s
-
-  influxdb/buffers:
-    endpoint: http://influxdb-b:8086
-    org: docs
-    bucket: buffers
-    token: mytoken123456789
-    timeout: 10s
-
-  debug:
-    verbosity: detailed
-
-service:
-  telemetry:
-    logs:
-      level: "debug"
-  pipelines:
-    metrics/ports:
-      receivers: [otlp]
-      processors: [batch, filter/ports]
-      exporters: [debug, influxdb/ports]
-    metrics/buffers:
-      receivers: [otlp]
-      processors: [batch, filter/buffers]
-      exporters: [debug, influxdb/buffers]
-    logs:
-      receivers: [otlp]
-      processors: [batch]
-      exporters: [debug]
-    traces:
-      receivers: [otlp]
-      processors: [batch]
-      exporters: [debug]
-EOF
-else
-    # Generate config from ConfigDB template
-    OTEL_VARS=$(sonic-cfggen -d -t $OTEL_VARS_FILE)
-    echo "$OTEL_VARS" > $CONFIG_FILE
+    echo "OTEL vars template file not found"
+    exit $EXIT_OTEL_VARS_FILE_NOT_FOUND
 fi
+
+# Read telemetry from ConfigDB
+echo "Generating OTEL config from template..."
+OTEL_CONFIG=$(sonic-cfggen -d -t $OTEL_VARS_FILE)
+
+if [ $? -ne 0 ] || [ -z "$OTEL_CONFIG" ]; then
+    echo "Failed to generate config from template, this should not happen"
+    exit 1
+fi
+
+echo "$OTEL_CONFIG" > $CONFIG_FILE
 
 OTEL_ARGS="--config=$CONFIG_FILE"
 
