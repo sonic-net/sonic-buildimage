8b7f5e81ec4346bba7dad3f061b1b7be7e86fb3c
bhaveshdell <55215495+bhaveshdell@users.noreply.github.com>
2025-09-04 23:59:21 +0100

    Address review comments. Avoid using namespace std; in a header file to prevent namespace pollution; qualify standard types explicitly. Avoid non-async-signal-safe function inside signal handler.

--- src/sonic-eventd/src/eventutils.h
+++ src/sonic-eventd/src/eventutils.h
@@ -4,21 +4,19 @@
 #include <string>
 #include <unordered_map>
 
-using namespace std;
+const std::string EVENT_SEVERITY_CRITICAL_STR = "CRITICAL";
+const std::string EVENT_SEVERITY_MAJOR_STR    = "MAJOR";
+const std::string EVENT_SEVERITY_MINOR_STR    = "MINOR";
+const std::string EVENT_SEVERITY_WARNING_STR  = "WARNING";
+const std::string EVENT_SEVERITY_INFORMATIONAL_STR = "INFORMATIONAL";
 
-const string EVENT_SEVERITY_CRITICAL_STR = "CRITICAL";
-const string EVENT_SEVERITY_MAJOR_STR    = "MAJOR";
-const string EVENT_SEVERITY_MINOR_STR    = "MINOR";
-const string EVENT_SEVERITY_WARNING_STR  = "WARNING";
-const string EVENT_SEVERITY_INFORMATIONAL_STR = "INFORMATIONAL";
+const std::string EVENT_ENABLE_TRUE_STR  = "true";
+const std::string EVENT_ENABLE_FALSE_STR = "false";
 
-const string EVENT_ENABLE_TRUE_STR  = "true";
-const string EVENT_ENABLE_FALSE_STR = "false";
-
-const string EVENT_ACTION_RAISE_STR  = "RAISE";
-const string EVENT_ACTION_CLEAR_STR  = "CLEAR";
-const string EVENT_ACTION_ACK_STR    = "ACKNOWLEDGE";
-const string EVENT_ACTION_UNACK_STR  = "UNACKNOWLEDGE";
+const std::string EVENT_ACTION_RAISE_STR  = "RAISE";
+const std::string EVENT_ACTION_CLEAR_STR  = "CLEAR";
+const std::string EVENT_ACTION_ACK_STR    = "ACKNOWLEDGE";
+const std::string EVENT_ACTION_UNACK_STR  = "UNACKNOWLEDGE";
 
 constexpr char EVENTD_DEFAULT_MAP_FILE[] = "/etc/evprofile/default.json";
 
@@ -27,16 +25,16 @@ constexpr size_t EHT_MAX_DAYS     = 30;
 constexpr char EVENTD_CONF_FILE[] = "/etc/eventd.json";
 
 typedef struct EventInfo_t {
-    string  severity;
-    string  enable;
-    string  static_event_msg;
+    std::string  severity;
+    std::string  enable;
+    std::string  static_event_msg;
 } EventInfo;
 
 //unordered_map<string, EventInfo> static_event_table;
-typedef unordered_map<string, EventInfo> EventMap; 
+typedef std::unordered_map<std::string, EventInfo> EventMap; 
 
-bool isValidSeverity(string severityStr);
-bool isValidEnable(string enableStr);
+bool isValidSeverity(std::string severityStr);
+bool isValidEnable(std::string enableStr);
 bool parse_config(const char *filename, unsigned int& days, unsigned int& count);
 bool parse(const char *filename, EventMap& tmp_event_table);
 
