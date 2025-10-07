65d1865b3ed3a405764814ca7a04c9a3941a6abf
Roy Wen <roy@nexthop.ai>
2025-10-07 16:11:16 -0400

    more nh-5010 device dir changes

--- device/nexthop/x86_64-nexthop_5010-r0/plugins/led_control.py
+++ device/nexthop/x86_64-nexthop_5010-r0/plugins/led_control.py
@@ -1,9 +1,1 @@
-# Copyright 2025 Nexthop Systems Inc. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# Nexthop Port LED Policy
-
-try:
-    from nexthop.led_control import LedControl
-except ImportError as e:
-    raise ImportError("%s - required module not found" % e)
+../../common/plugins/led_control.py
