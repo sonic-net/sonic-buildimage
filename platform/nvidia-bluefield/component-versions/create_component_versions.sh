a4917596849558b8e6970f60b90b0203faf5351f
Ben Levi <belevi@nvidia.com>
2025-12-16 08:54:53 +0200

    remove relative path
    Signed-off-by: Ben Levi <belevi@nvidia.com>

--- platform/nvidia-bluefield/component-versions/create_component_versions.sh
+++ platform/nvidia-bluefield/component-versions/create_component_versions.sh
@@ -20,7 +20,7 @@ echo "SDK $SDK_VERSION" > temp_versions_file
 echo "FW $BF3_FW_VERSION" >> temp_versions_file
 echo "SAI $DPU_SAI_VERSION" >> temp_versions_file
 
-SAI_API_VERSION_PATH="../../../src/sonic-sairedis/SAI/inc/saiversion.h"
+SAI_API_VERSION_PATH="/sonic/src/sonic-sairedis/SAI/inc/saiversion.h"
 if [ -f "$SAI_API_VERSION_PATH" ]; then
     SAI_MAJOR=$(grep "SAI_MAJOR" "$SAI_API_VERSION_PATH" | grep -oE "[0-9]+")
     SAI_MINOR=$(grep "SAI_MINOR" "$SAI_API_VERSION_PATH" | grep -oE "[0-9]+")
