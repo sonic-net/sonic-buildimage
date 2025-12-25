cf6827ebaa18cbadca6c912ef94330733de6b941
Ben Levi <belevi@nvidia.com>
2025-12-25 11:21:50 +0200

    BMC compilation variables
    Signed-off-by: Ben Levi <belevi@nvidia.com>

--- platform/mellanox/mlnx-platform-api/setup.py
+++ platform/mellanox/mlnx-platform-api/setup.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019-2025 NVIDIA CORPORATION & AFFILIATES.
+# Copyright (c) 2019-2024 NVIDIA CORPORATION & AFFILIATES.
 # Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,8 +35,7 @@ setup(
         'pytest-runner'
     ],
     install_requires= [
-        'inotify',
-        'filelock'
+        'inotify'
     ],
     tests_require = [
         'pytest',
