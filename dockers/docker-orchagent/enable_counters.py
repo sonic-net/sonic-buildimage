c5aca739bc3c603cbc9fe187332dccde83ea2a17
Connor Roos <croos@nvidia.com>
2026-02-10 02:29:03 +0200

    Add logic for ha-set counters
    Signed-off-by: Connor Roos <croos@nvidia.com>

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -37,7 +37,7 @@ def enable_rates():
 def enable_counters():
     db = swsscommon.ConfigDBConnector()
     db.connect()
-    dpu_counters = ["ENI","DASH_METER"]
+    dpu_counters = ["ENI","DASH_METER", "CP_DATA_CHANNEL", "BULK_SYNC"]
 
     platform_info = device_info.get_platform_info(db)
     if platform_info.get('switch_type') == 'dpu':
