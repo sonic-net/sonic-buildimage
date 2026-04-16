3bdb7bf8cf820ae8c4b5c31512c9b23395b864f5
Mavroudis Chatzilazaridis <mavchatz@protonmail.com>
2026-04-16 15:38:21 +0300

    sonic-rs-common: Add SimpleLogger struct
    This commit adds a struct implementing the Log trait from the log crate.
    
    This allows Rust applications to use the macros from the log crate to
    write messages to syslog.
    
    Signed-off-by: Mavroudis Chatzilazaridis <mavchatz@protonmail.com>

--- src/sonic-rs-common/src/lib.rs
+++ src/sonic-rs-common/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod device_info;
 pub mod logger;
+pub mod simple_logger;
 
 pub use device_info::{DeviceInfoError, is_warm_restart_enabled, is_fast_reboot_enabled};
 pub use logger::{Logger, LoggerError, LoggerResult};
+pub use simple_logger::SimpleLogger;
