a4935b0aa64d67327bf2af1c5f34aefd6e69e531
Hossam Hassan <hossam4.hassan@orange.com>
2026-04-24 10:53:21 +0300

    fix startup script arg handling and timer description
    - Use `// empty` in jq expressions to avoid appending literal "null"
      for absent port/log_level fields in NETCONF_SERVER_ARGS
    - Fix netconf-server.timer description to reference the correct container
    - Change INCLUDE_NETCONF_SERVER default to `?= n` to allow override
      without forcing the value
    
    Signed-off-by: Hossam Hassan <hossam4.hassan@orange.com>

--- dockers/docker-sonic-netconf-server/netconf-server.sh
+++ dockers/docker-sonic-netconf-server/netconf-server.sh
@@ -13,19 +13,19 @@ fi
 MGMT_VARS=$(sonic-cfggen -d -t $MGMT_VARS_FILE)
 MGMT_VARS=${MGMT_VARS//[\']/\"}
 
-NETCONF_SERVER=$(echo $MGMT_VARS | jq -r '.netconf_server')
+NETCONF_SERVER=$(echo "$MGMT_VARS" | jq -r '.netconf_server // empty')
 
 if [ -n "$NETCONF_SERVER" ]; then
-    SERVER_PORT=$(echo $NETCONF_SERVER | jq -r '.port')
-    LOG_LEVEL=$(echo $NETCONF_SERVER | jq -r '.log_level')
+    SERVER_PORT=$(echo "$NETCONF_SERVER" | jq -r '.port // empty')
+    LOG_LEVEL=$(echo "$NETCONF_SERVER" | jq -r '.log_level // empty')
 else
     SERVER_PORT=830
     LOG_LEVEL=5
 fi
 
 NETCONF_SERVER_ARGS="-logtostderr"
-[ ! -z $SERVER_PORT ] && NETCONF_SERVER_ARGS+=" -port $SERVER_PORT"
-[ ! -z $LOG_LEVEL   ] && NETCONF_SERVER_ARGS+=" -v $LOG_LEVEL"
+[ -n "$SERVER_PORT" ] && NETCONF_SERVER_ARGS+=" -port $SERVER_PORT"
+[ -n "$LOG_LEVEL"   ] && NETCONF_SERVER_ARGS+=" -v $LOG_LEVEL"
 
 echo "NETCONF_SERVER_ARGS = $NETCONF_SERVER_ARGS"
 
