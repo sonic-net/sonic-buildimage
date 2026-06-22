094e37aa6225f667bf07b0fd4354420bc754bf12
Borja Lorente <blorente.me@gmail.com>
2026-06-22 14:01:36 +0100

    build: Test docker image building on the slave
    Signed-off-by: Borja Lorente <blorente.me@gmail.com>

--- tools/bazel/test_working_targets.sh
+++ tools/bazel/test_working_targets.sh
@@ -3,13 +3,28 @@ set -euo pipefail
 
 repo_root=$(git rev-parse --show-toplevel)
 
+function run_in_slave() {
+  repo=$1
+  cmd=$2
+
+  # Run the same command inside the sonic-slave-bookworm container.
+  if [[ "${SKIP_SLAVE:-0}" != "1" ]]; then
+    echo "[slave] ${repo}: ${cmd}"
+    make -C "${repo_root}" -f Makefile.work BLDENV=bookworm sonic-slave-run \
+      SONIC_RUN_CMDS="cd /sonic/${repo} && ${cmd}"
+  fi
+}
+
 function test_repo() {
   repo=$1
   cmd=$2
-  echo "[test] ${repo}: ${cmd}"
+
+  echo "[host] ${repo}: ${cmd}"
   pushd "${repo_root}/${repo}"
   ${cmd}
   popd
+
+  run_in_slave $1 $2
 }
 
 echo "[= Testing Dependent Repositories =]"
@@ -18,15 +33,29 @@ test_repo "src/sonic-build-infra" "bazel build ..."
 test_repo "src/sonic-swss-common" "bazel build ..."
 test_repo "src/sonic-sysmgr" "bazel build ..."
 test_repo "src/libnl3" "bazel build ..."
+test_repo "src/libyang3" "bazel build ..."
 
 echo "[= Testing Docker Images =]"
 
 cd "${repo_root}"
-bazel query 'kind(oci_load, ...)'
+set +e
+docker_images=$(
+  bazel query --keep_going 'kind(oci_image, ...)' 2>/dev/null
+)
+docker_loads=$(
+  bazel query --keep_going 'kind(oci_load, ...)' 2>/dev/null
+)
+set -e
 
-for load in $(bazel query 'kind(oci_load, ...)'); do
+for load in ${docker_loads[@]}; do
     echo "[load] ${load}"
     bazel run "${load}"
 done
+
+for image in ${docker_images[@]}; do
+    echo "[load] ${load}"
+    run_in_slave "." "bazel build ${image}"
+done
+
 
 echo "[= DONE =]"
