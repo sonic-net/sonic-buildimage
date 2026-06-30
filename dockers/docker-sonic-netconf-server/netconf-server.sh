c2776fa92701e8b3fadcb3cbe23762f4e63d8bc6
Hossam Hassan <hossam4.hassan@orange.com>
2026-06-30 09:46:44 +0300

    Configure NETCONF host key algorithm
    Pass NETCONF host key algorithm and RSA size settings from container config to the server, defaulting to ed25519 and RSA 3072 bits.
    
    Signed-off-by: Hossam Hassan <hossam4.hassan@orange.com>

--- dockers/docker-sonic-netconf-server/netconf-server.sh
+++ dockers/docker-sonic-netconf-server/netconf-server.sh
@@ -18,14 +18,21 @@ NETCONF_SERVER=$(echo "$MGMT_VARS" | jq -r '.netconf_server // empty')
 if [ -n "$NETCONF_SERVER" ]; then
     SERVER_PORT=$(echo "$NETCONF_SERVER" | jq -r '.port // empty')
     LOG_LEVEL=$(echo "$NETCONF_SERVER" | jq -r '.log_level // empty')
+    HOST_KEY_ALGORITHM=$(echo "$NETCONF_SERVER" | jq -r '.host_key_algorithm // empty')
+    HOST_KEY_RSA_BITS=$(echo "$NETCONF_SERVER" | jq -r '.host_key_rsa_bits // empty')
 else
     SERVER_PORT=830
     LOG_LEVEL=5
 fi
 
+[ -z "$HOST_KEY_ALGORITHM" ] && HOST_KEY_ALGORITHM=ed25519
+[ -z "$HOST_KEY_RSA_BITS"  ] && HOST_KEY_RSA_BITS=3072
+
 NETCONF_SERVER_ARGS=("-logtostderr")
 [ -n "$SERVER_PORT" ] && NETCONF_SERVER_ARGS+=("-port" "$SERVER_PORT")
 [ -n "$LOG_LEVEL"   ] && NETCONF_SERVER_ARGS+=("-v" "$LOG_LEVEL")
+[ -n "$HOST_KEY_ALGORITHM" ] && NETCONF_SERVER_ARGS+=("-host_key_algorithm" "$HOST_KEY_ALGORITHM")
+[ -n "$HOST_KEY_RSA_BITS"  ] && NETCONF_SERVER_ARGS+=("-host_key_rsa_bits" "$HOST_KEY_RSA_BITS")
 
 echo "NETCONF_SERVER_ARGS = ${NETCONF_SERVER_ARGS[*]}"
 
