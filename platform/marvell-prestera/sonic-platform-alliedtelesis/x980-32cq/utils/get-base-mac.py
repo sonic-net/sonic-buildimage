f10109f08589aa69205c8410451ffdc0a414379c
Chris Packham <chris.packham@alliedtelesis.co.nz>
2026-07-09 15:38:00 +1200

    [marvell-prestera]: alliedtelesis: More python3 updates
    Update some Python 2 code that was missed earlier.
    
    Signed-off-by: Chris Packham <chris.packham@alliedtelesis.co.nz>

--- platform/marvell-prestera/sonic-platform-alliedtelesis/x980-32cq/utils/get-base-mac.py
+++ platform/marvell-prestera/sonic-platform-alliedtelesis/x980-32cq/utils/get-base-mac.py
@@ -4,16 +4,16 @@
 def main():
     try:
         import sonic_platform.platform
-        import sonic_platform.chassis
     except ImportError as e:
         raise ImportError(str(e) + "- required module not found")
 
     chassis = sonic_platform.platform.Platform().get_chassis()
     if chassis is None:
-        print "DEBUG chassis was None  "
+        print("DEBUG chassis was None  ")
+        return
 
     base_mac = chassis.get_base_mac()
-    print base_mac
+    print(base_mac)
 
     return
 
