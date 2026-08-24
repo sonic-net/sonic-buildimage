c02037999e1d78aab80715c5e35fe629c634cd21
Natarajan Subbiramani <natarajan.subbiramani@nokia.com>
2026-08-23 23:27:41 -0400

    [Nokia-BMC-H6-128] Add eeprom, chassis APIs
    Signed-off-by: Natarajan Subbiramani <natarajan.subbiramani@nokia.com>

--- platform/aspeed/sonic-platform-modules-nokia/h6-128/sonic_platform/chassis.py
+++ platform/aspeed/sonic-platform-modules-nokia/h6-128/sonic_platform/chassis.py
@@ -334,6 +334,15 @@ class Chassis(ChassisBase):
         """
         return None
 
+    def get_position_in_parent(self):
+        """
+        Retrieves 1-based relative physical position in parent device.
+        Returns:
+            integer: The 1-based relative physical position in parent
+            device or -1 if cannot determine the position
+        """
+        return -1
+
     def is_replaceable(self):
         """
         Indicate whether this device is replaceable.
