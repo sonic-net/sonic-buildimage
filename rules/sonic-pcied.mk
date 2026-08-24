59aaaf62c4db7746365358d0b33e291678ffd20e
Dennis Lanov <dennis.lanov@gmail.com>
2026-08-24 13:50:40 -0500

    Extend nested patch paths to related packages
    Signed-off-by: Dennis Lanov <dennis.lanov@gmail.com>

--- rules/sonic-pcied.mk
+++ rules/sonic-pcied.mk
@@ -4,6 +4,7 @@
 
 SONIC_PCIED_PY3 = sonic_pcied-1.0-py3-none-any.whl
 $(SONIC_PCIED_PY3)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-pcied
+$(SONIC_PCIED_PY3)_PATCH_PATH = $(SRC_PATH)/sonic-platform-daemons.patch/sonic-pcied
 $(SONIC_PCIED_PY3)_DEPENDS = $(SONIC_PY_COMMON_PY3)
 $(SONIC_PCIED_PY3)_PYTHON_VERSION = 3
 SONIC_PYTHON_WHEELS += $(SONIC_PCIED_PY3)
--- rules/sonic-stormond.mk
+++ rules/sonic-stormond.mk
@@ -4,6 +4,7 @@
 
 SONIC_STORMOND_PY3 = sonic_stormond-1.0-py3-none-any.whl
 $(SONIC_STORMOND_PY3)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-stormond
+$(SONIC_STORMOND_PY3)_PATCH_PATH = $(SRC_PATH)/sonic-platform-daemons.patch/sonic-stormond
 $(SONIC_STORMOND_PY3)_DEPENDS = $(SONIC_PY_COMMON_PY3)
 $(SONIC_STORMOND_PY3)_PYTHON_VERSION = 3
 SONIC_PYTHON_WHEELS += $(SONIC_STORMOND_PY3)
