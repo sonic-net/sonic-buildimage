d1c4dbe1dedd2c2c2cf1134b344616e773e71e99
Ashutosh Agrawal <ashu@cisco.com>
2026-08-27 10:55:10 -0700

    bgpcfgd: Validate DEVICE_METADATA BGP ASN
    Validate the localhost BGP ASN before publishing DEVICE_METADATA updates to dependent managers.
    
    Signed-off-by: Ashutosh Agrawal <ashu@cisco.com>

--- src/sonic-bgpcfgd/bgpcfgd/managers_db.py
+++ src/sonic-bgpcfgd/bgpcfgd/managers_db.py
@@ -1,3 +1,6 @@
+from swsscommon import swsscommon
+
+from .log import log_err
 from .manager import Manager
 
 
@@ -19,6 +22,12 @@ class BGPDataBaseMgr(Manager):
 
     def set_handler(self, key, data):
         """ Implementation of 'SET' command for this class """
+        if (self.table_name == swsscommon.CFG_DEVICE_METADATA_TABLE_NAME
+                and key == "localhost" and data and "bgp_asn" in data
+                and not self.__is_valid_asn(data["bgp_asn"])):
+            log_err("BGPDataBaseMgr::Invalid bgp_asn value: %r. Row ignored" % data["bgp_asn"])
+            return True
+
         self.directory.put(self.db_name, self.table_name, key, data)
 
         return True
@@ -26,3 +35,11 @@ class BGPDataBaseMgr(Manager):
     def del_handler(self, key):
         """ Implementation of 'DEL' command for this class """
         self.directory.remove(self.db_name, self.table_name, key)
+
+    @staticmethod
+    def __is_valid_asn(value):
+        """Return True when value is a decimal BGP autonomous system number."""
+        if not isinstance(value, str) or not value.isascii() or not value.isdigit():
+            return False
+
+        return 0 < int(value) <= 0xffffffff
