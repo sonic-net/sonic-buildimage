e3e4ad2215af94321341a6e8b1226e4aa836f2c8
Ashutosh Agrawal <ashu@cisco.com>
2026-08-27 08:18:45 -0700

    frr,gobgp: disable Zebra TCP VTY listeners
    Signed-off-by: Ashutosh Agrawal <ashu@cisco.com>

--- dockers/docker-fpm-frr/zsocket.sh
+++ dockers/docker-fpm-frr/zsocket.sh
@@ -1,36 +1,12 @@
 #!/usr/bin/env bash
 
-addr="127.0.0.1"
-port=2601
-
-function help()
-{
-    echo "This script aims to ensure zebra is ready to accept connections"
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo " -a   Zebra address"
-    echo " -o   Zebra port"
-    exit 1
-}
-
-while getopts ":a:o:h" opt; do
-    case "${opt}" in
-        h) help
-            ;;
-        a) addr=${OPTARG}
-            ;;
-        o) port=${OPTARG}
-            ;;
-    esac
-done
-
 start=$(date +%s.%N)
-timeout 5s bash -c -- "until </dev/tcp/${addr}/${port}; do sleep 0.1;done"
+timeout 5s bash -c -- 'until vtysh -d zebra -c "show version" >/dev/null 2>&1; do sleep 0.1; done'
 if [ "$?" != "0" ]; then
-    logger -p error "Error: zebra is not ready to accept connections"
+    logger -p error "Error: zebra is not ready on its vtysh socket"
 else
     timespan=$(awk "BEGIN {print $(date +%s.%N)-$start; exit}")
-    logger -p info "It took ${timespan} seconds to wait for zebra to be ready to accept connections"
+    logger -p info "It took ${timespan} seconds to wait for zebra's vtysh socket"
 fi
 
 exit 0
