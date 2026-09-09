e550b749b9b5896da0a83febfc2a46b5f2e4e9f4
donghaolicd <leedonhom@gmail.com>
2026-09-09 21:25:10 +0000

    [docker-sonic-mgmt] Limit GHZ change to installation
    Signed-off-by: donghaolicd <leedonhom@gmail.com>

--- rules/docker-sonic-mgmt.mk
+++ rules/docker-sonic-mgmt.mk
@@ -18,20 +18,6 @@ $(error Unsupported docker-sonic-mgmt architecture: $(CONFIGURED_ARCH))
 endif
 GO_TOOLCHAIN = go$(GO_VERSION).linux-$(GO_ARCHIVE_ARCH).tar.gz
 $(GO_TOOLCHAIN)_URL = https://go.dev/dl/$(GO_TOOLCHAIN)
-UV_VERSION = 0.12.10
-ifeq ($(CONFIGURED_ARCH),amd64)
-UV_TARGET = x86_64-unknown-linux-gnu
-else ifeq ($(CONFIGURED_ARCH),arm64)
-UV_TARGET = aarch64-unknown-linux-gnu
-else ifeq ($(CONFIGURED_ARCH),armhf)
-UV_TARGET = armv7-unknown-linux-gnueabihf
-endif
-UV_ARCHIVE = uv-$(UV_VERSION)-$(UV_TARGET).tar.gz
-$(UV_ARCHIVE)_URL = https://github.com/astral-sh/uv/releases/download/$(UV_VERSION)/uv-$(UV_TARGET).tar.gz
-UV_LICENSE_MIT = uv-$(UV_VERSION)-LICENSE-MIT
-$(UV_LICENSE_MIT)_URL = https://raw.githubusercontent.com/astral-sh/uv/$(UV_VERSION)/LICENSE-MIT
-UV_LICENSE_APACHE = uv-$(UV_VERSION)-LICENSE-APACHE
-$(UV_LICENSE_APACHE)_URL = https://raw.githubusercontent.com/astral-sh/uv/$(UV_VERSION)/LICENSE-APACHE
 GNMI_PROTO_VERSION = 0.14.1
 GNMI_PROTO = gnmi-v$(GNMI_PROTO_VERSION).proto
 $(GNMI_PROTO)_URL = https://raw.githubusercontent.com/openconfig/gnmi/v$(GNMI_PROTO_VERSION)/proto/gnmi/gnmi.proto
@@ -42,13 +28,12 @@ $(GNMI_LICENSE)_URL = https://raw.githubusercontent.com/openconfig/gnmi/v$(GNMI_
 CONFIGOR_LICENSE_COMMIT = f7a0fc7c9fc697269b1749c1cc472d49baa8d33c
 CONFIGOR_LICENSE = configor-$(CONFIGOR_LICENSE_COMMIT)-LICENSE
 $(CONFIGOR_LICENSE)_URL = https://raw.githubusercontent.com/jinzhu/configor/$(CONFIGOR_LICENSE_COMMIT)/LICENSE
-SONIC_ONLINE_FILES += $(GHZ_SOURCE) $(GO_TOOLCHAIN) $(UV_ARCHIVE) $(UV_LICENSE_MIT) $(UV_LICENSE_APACHE) \
+SONIC_ONLINE_FILES += $(GHZ_SOURCE) $(GO_TOOLCHAIN) \
                       $(GNMI_PROTO) $(GNMI_EXT_PROTO) $(GNMI_LICENSE) $(CONFIGOR_LICENSE)
 
 $(DOCKER_SONIC_MGMT)_PATH = $(DOCKERS_PATH)/docker-sonic-mgmt
 $(DOCKER_SONIC_MGMT)_DEPENDS += $(SONIC_DEVICE_DATA)
-$(DOCKER_SONIC_MGMT)_FILES += $(GITHUB_GET) $(GHZ_SOURCE) $(GO_TOOLCHAIN) $(UV_ARCHIVE) \
-                              $(UV_LICENSE_MIT) $(UV_LICENSE_APACHE) \
+$(DOCKER_SONIC_MGMT)_FILES += $(GITHUB_GET) $(GHZ_SOURCE) $(GO_TOOLCHAIN) \
                               $(GNMI_PROTO) $(GNMI_EXT_PROTO) $(GNMI_LICENSE) $(CONFIGOR_LICENSE)
 SONIC_DOCKER_IMAGES += $(DOCKER_SONIC_MGMT)
 SONIC_BOOKWORM_DOCKERS += $(DOCKER_SONIC_MGMT)
